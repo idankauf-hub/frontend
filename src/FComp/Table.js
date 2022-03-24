a0ac901d738af76fa943daae1e7306fd54785bc6
idankauf <idankauf@gmail.com>
2022-03-24 18:44:45 +0200

    frontend table finsh

---
+++ src/FComp/Table.js
@@ -0,0 +1,29 @@
+import { DataGrid } from '@mui/x-data-grid'
+import React,{useState,useEffect} from 'react';
+import Data from '../data.json'
+import './table.css';
+export default function Table() {
+  const [dataRow,setData]=useState([Data]);
+
+  const columns = [
+    { field: 'asset', headerName: 'Asset', width: 70 },
+    { field: 'ip', headerName: 'IP Address', width: 120 },
+    
+  ];
+  
+  useEffect(()=>{
+  },[dataRow])
+
+    return (
+      <div className='table' style={{ height: 400, width: '80%'}}>
+        <DataGrid
+          rows={dataRow[0]}
+          columns={columns}
+          pageSize={10}
+          rowsPerPageOptions={[10]}
+          getRowId={(row) => row.asset}
+        />
+     
+      </div>
+    );
+}
